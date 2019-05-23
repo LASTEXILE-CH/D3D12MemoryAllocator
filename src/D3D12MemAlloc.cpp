b7b976b85bd16fd574ce0a84d5c6eca50e45c1e4
Adam Sawicki <Adam.Sawicki@amd.com>
2019-05-23 11:07:23 +0200

    Add private class List - doubly linked list
    Also add test for it.

--- src/D3D12MemAlloc.cpp
+++ src/D3D12MemAlloc.cpp
@@ -686,7 +686,7 @@ private:
 };
 
 ////////////////////////////////////////////////////////////////////////////////
-// private class PoolAllocator
+// Private class PoolAllocator
 
 /*
 Allocator for objects of type T using a list of arrays (pools) to speed up
@@ -700,6 +700,7 @@ class PoolAllocator
 {
     D3D12MA_CLASS_NO_COPY(PoolAllocator)
 public:
+    // allocationCallbacks externally owned, must outlive this object.
     PoolAllocator(const ALLOCATION_CALLBACKS& allocationCallbacks, uint32_t firstBlockCapacity);
     ~PoolAllocator() { Clear(); }
     void Clear();
@@ -814,6 +815,463 @@ typename PoolAllocator<T>::ItemBlock& PoolAllocator<T>::CreateNewBlock()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Private class List
+
+/*
+Doubly linked list, with elements allocated out of PoolAllocator.
+Has custom interface, as well as STL-style interface, including iterator and
+const_iterator.
+*/
+template<typename T>
+class List
+{
+    D3D12MA_CLASS_NO_COPY(List)
+public:
+    struct Item
+    {
+        Item* pPrev;
+        Item* pNext;
+        T Value;
+    };
+
+    // allocationCallbacks externally owned, must outlive this object.
+    List(const ALLOCATION_CALLBACKS& allocationCallbacks);
+    ~List();
+    void Clear();
+
+    size_t GetCount() const { return m_Count; }
+    bool IsEmpty() const { return m_Count == 0; }
+
+    Item* Front() { return m_pFront; }
+    const Item* Front() const { return m_pFront; }
+    Item* Back() { return m_pBack; }
+    const Item* Back() const { return m_pBack; }
+
+    Item* PushBack();
+    Item* PushFront();
+    Item* PushBack(const T& value);
+    Item* PushFront(const T& value);
+    void PopBack();
+    void PopFront();
+
+    // Item can be null - it means PushBack.
+    Item* InsertBefore(Item* pItem);
+    // Item can be null - it means PushFront.
+    Item* InsertAfter(Item* pItem);
+
+    Item* InsertBefore(Item* pItem, const T& value);
+    Item* InsertAfter(Item* pItem, const T& value);
+
+    void Remove(Item* pItem);
+
+    class iterator
+    {
+    public:
+        iterator() :
+            m_pList(NULL),
+            m_pItem(NULL)
+        {
+        }
+
+        T& operator*() const
+        {
+            D3D12MA_HEAVY_ASSERT(m_pItem != NULL);
+            return m_pItem->Value;
+        }
+        T* operator->() const
+        {
+            D3D12MA_HEAVY_ASSERT(m_pItem != NULL);
+            return &m_pItem->Value;
+        }
+
+        iterator& operator++()
+        {
+            D3D12MA_HEAVY_ASSERT(m_pItem != NULL);
+            m_pItem = m_pItem->pNext;
+            return *this;
+        }
+        iterator& operator--()
+        {
+            if(m_pItem != NULL)
+            {
+                m_pItem = m_pItem->pPrev;
+            }
+            else
+            {
+                D3D12MA_HEAVY_ASSERT(!m_pList->IsEmpty());
+                m_pItem = m_pList->Back();
+            }
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            iterator result = *this;
+            ++*this;
+            return result;
+        }
+        iterator operator--(int)
+        {
+            iterator result = *this;
+            --*this;
+            return result;
+        }
+
+        bool operator==(const iterator& rhs) const
+        {
+            D3D12MA_HEAVY_ASSERT(m_pList == rhs.m_pList);
+            return m_pItem == rhs.m_pItem;
+        }
+        bool operator!=(const iterator& rhs) const
+        {
+            D3D12MA_HEAVY_ASSERT(m_pList == rhs.m_pList);
+            return m_pItem != rhs.m_pItem;
+        }
+
+    private:
+        List<T>* m_pList;
+        List<T>::Item* m_pItem;
+
+        iterator(List<T>* pList, List<T>::Item* pItem) :
+            m_pList(pList),
+            m_pItem(pItem)
+        {
+        }
+
+        friend class List<T>;
+    };
+
+    class const_iterator
+    {
+    public:
+        const_iterator() :
+            m_pList(NULL),
+            m_pItem(NULL)
+        {
+        }
+
+        const_iterator(const iterator& src) :
+            m_pList(src.m_pList),
+            m_pItem(src.m_pItem)
+        {
+        }
+
+        const T& operator*() const
+        {
+            D3D12MA_HEAVY_ASSERT(m_pItem != NULL);
+            return m_pItem->Value;
+        }
+        const T* operator->() const
+        {
+            D3D12MA_HEAVY_ASSERT(m_pItem != NULL);
+            return &m_pItem->Value;
+        }
+
+        const_iterator& operator++()
+        {
+            D3D12MA_HEAVY_ASSERT(m_pItem != NULL);
+            m_pItem = m_pItem->pNext;
+            return *this;
+        }
+        const_iterator& operator--()
+        {
+            if(m_pItem != NULL)
+            {
+                m_pItem = m_pItem->pPrev;
+            }
+            else
+            {
+                D3D12MA_HEAVY_ASSERT(!m_pList->IsEmpty());
+                m_pItem = m_pList->Back();
+            }
+            return *this;
+        }
+
+        const_iterator operator++(int)
+        {
+            const_iterator result = *this;
+            ++*this;
+            return result;
+        }
+        const_iterator operator--(int)
+        {
+            const_iterator result = *this;
+            --*this;
+            return result;
+        }
+
+        bool operator==(const const_iterator& rhs) const
+        {
+            D3D12MA_HEAVY_ASSERT(m_pList == rhs.m_pList);
+            return m_pItem == rhs.m_pItem;
+        }
+        bool operator!=(const const_iterator& rhs) const
+        {
+            D3D12MA_HEAVY_ASSERT(m_pList == rhs.m_pList);
+            return m_pItem != rhs.m_pItem;
+        }
+
+    private:
+        const_iterator(const List<T>* pList, const List<T>::Item* pItem) :
+            m_pList(pList),
+            m_pItem(pItem)
+        {
+        }
+
+        const List<T>* m_pList;
+        const List<T>::Item* m_pItem;
+
+        friend class List<T>;
+    };
+
+    bool empty() const { return IsEmpty(); }
+    size_t size() const { return GetCount(); }
+
+    iterator begin() { return iterator(this, Front()); }
+    iterator end() { return iterator(this, NULL); }
+
+    const_iterator cbegin() const { return const_iterator(this, Front()); }
+    const_iterator cend() const { return const_iterator(this, NULL); }
+
+    void clear() { Clear(); }
+    void push_back(const T& value) { PushBack(value); }
+    void erase(iterator it) { Remove(it.m_pItem); }
+    iterator insert(iterator it, const T& value) { return iterator(this, InsertBefore(it.m_pItem, value)); }
+
+private:
+    const ALLOCATION_CALLBACKS& m_AllocationCallbacks;
+    PoolAllocator<Item> m_ItemAllocator;
+    Item* m_pFront;
+    Item* m_pBack;
+    size_t m_Count;
+};
+
+template<typename T>
+List<T>::List(const ALLOCATION_CALLBACKS& allocationCallbacks) :
+    m_AllocationCallbacks(allocationCallbacks),
+    m_ItemAllocator(allocationCallbacks, 128),
+    m_pFront(NULL),
+    m_pBack(NULL),
+    m_Count(0)
+{
+}
+
+template<typename T>
+List<T>::~List()
+{
+    // Intentionally not calling Clear, because that would be unnecessary
+    // computations to return all items to m_ItemAllocator as free.
+}
+
+template<typename T>
+void List<T>::Clear()
+{
+    if(!IsEmpty())
+    {
+        Item* pItem = m_pBack;
+        while(pItem != NULL)
+        {
+            Item* const pPrevItem = pItem->pPrev;
+            m_ItemAllocator.Free(pItem);
+            pItem = pPrevItem;
+        }
+        m_pFront = NULL;
+        m_pBack = NULL;
+        m_Count = 0;
+    }
+}
+
+template<typename T>
+typename List<T>::Item* List<T>::PushBack()
+{
+    Item* const pNewItem = m_ItemAllocator.Alloc();
+    pNewItem->pNext = NULL;
+    if(IsEmpty())
+    {
+        pNewItem->pPrev = NULL;
+        m_pFront = pNewItem;
+        m_pBack = pNewItem;
+        m_Count = 1;
+    }
+    else
+    {
+        pNewItem->pPrev = m_pBack;
+        m_pBack->pNext = pNewItem;
+        m_pBack = pNewItem;
+        ++m_Count;
+    }
+    return pNewItem;
+}
+
+template<typename T>
+typename List<T>::Item* List<T>::PushFront()
+{
+    Item* const pNewItem = m_ItemAllocator.Alloc();
+    pNewItem->pPrev = NULL;
+    if(IsEmpty())
+    {
+        pNewItem->pNext = NULL;
+        m_pFront = pNewItem;
+        m_pBack = pNewItem;
+        m_Count = 1;
+    }
+    else
+    {
+        pNewItem->pNext = m_pFront;
+        m_pFront->pPrev = pNewItem;
+        m_pFront = pNewItem;
+        ++m_Count;
+    }
+    return pNewItem;
+}
+
+template<typename T>
+typename List<T>::Item* List<T>::PushBack(const T& value)
+{
+    Item* const pNewItem = PushBack();
+    pNewItem->Value = value;
+    return pNewItem;
+}
+
+template<typename T>
+typename List<T>::Item* List<T>::PushFront(const T& value)
+{
+    Item* const pNewItem = PushFront();
+    pNewItem->Value = value;
+    return pNewItem;
+}
+
+template<typename T>
+void List<T>::PopBack()
+{
+    D3D12MA_HEAVY_ASSERT(m_Count > 0);
+    Item* const pBackItem = m_pBack;
+    Item* const pPrevItem = pBackItem->pPrev;
+    if(pPrevItem != NULL)
+    {
+        pPrevItem->pNext = NULL;
+    }
+    m_pBack = pPrevItem;
+    m_ItemAllocator.Free(pBackItem);
+    --m_Count;
+}
+
+template<typename T>
+void List<T>::PopFront()
+{
+    D3D12MA_HEAVY_ASSERT(m_Count > 0);
+    Item* const pFrontItem = m_pFront;
+    Item* const pNextItem = pFrontItem->pNext;
+    if(pNextItem != NULL)
+    {
+        pNextItem->pPrev = NULL;
+    }
+    m_pFront = pNextItem;
+    m_ItemAllocator.Free(pFrontItem);
+    --m_Count;
+}
+
+template<typename T>
+void List<T>::Remove(Item* pItem)
+{
+    D3D12MA_HEAVY_ASSERT(pItem != NULL);
+    D3D12MA_HEAVY_ASSERT(m_Count > 0);
+
+    if(pItem->pPrev != NULL)
+    {
+        pItem->pPrev->pNext = pItem->pNext;
+    }
+    else
+    {
+        D3D12MA_HEAVY_ASSERT(m_pFront == pItem);
+        m_pFront = pItem->pNext;
+    }
+
+    if(pItem->pNext != NULL)
+    {
+        pItem->pNext->pPrev = pItem->pPrev;
+    }
+    else
+    {
+        D3D12MA_HEAVY_ASSERT(m_pBack == pItem);
+        m_pBack = pItem->pPrev;
+    }
+
+    m_ItemAllocator.Free(pItem);
+    --m_Count;
+}
+
+template<typename T>
+typename List<T>::Item* List<T>::InsertBefore(Item* pItem)
+{
+    if(pItem != NULL)
+    {
+        Item* const prevItem = pItem->pPrev;
+        Item* const newItem = m_ItemAllocator.Alloc();
+        newItem->pPrev = prevItem;
+        newItem->pNext = pItem;
+        pItem->pPrev = newItem;
+        if(prevItem != NULL)
+        {
+            prevItem->pNext = newItem;
+        }
+        else
+        {
+            D3D12MA_HEAVY_ASSERT(m_pFront == pItem);
+            m_pFront = newItem;
+        }
+        ++m_Count;
+        return newItem;
+    }
+    else
+    {
+        return PushBack();
+    }
+}
+
+template<typename T>
+typename List<T>::Item* List<T>::InsertAfter(Item* pItem)
+{
+    if(pItem != NULL)
+    {
+        Item* const nextItem = pItem->pNext;
+        Item* const newItem = m_ItemAllocator.Alloc();
+        newItem->pNext = nextItem;
+        newItem->pPrev = pItem;
+        pItem->pNext = newItem;
+        if(nextItem != NULL)
+        {
+            nextItem->pPrev = newItem;
+        }
+        else
+        {
+            D3D12MA_HEAVY_ASSERT(m_pBack == pItem);
+            m_pBack = newItem;
+        }
+        ++m_Count;
+        return newItem;
+    }
+    else
+        return PushFront();
+}
+
+template<typename T>
+typename List<T>::Item* List<T>::InsertBefore(List<T>::Item* pItem, const T& value)
+{
+    Item* const newItem = InsertBefore(pItem);
+    newItem->Value = value;
+    return newItem;
+}
+
+template<typename T>
+typename List<T>::Item* List<T>::InsertAfter(List<T>::Item* pItem, const T& value)
+{
+    Item* const newItem = InsertAfter(pItem);
+    newItem->Value = value;
+    return newItem;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Private class AllocatorPimpl definition and implementation
 
 class AllocatorPimpl
@@ -867,12 +1325,13 @@ Allocator::~Allocator()
 
 void Allocator::Test()
 {
-    /*
+#if 0
     Vector<size_t> v(m_Pimpl->GetAllocationCallbacks());
     for(size_t i = 0; i < 1000; ++i)
         v.push_back(i);
-    */
+#endif
 
+#if 0
     PoolAllocator<size_t> poolAlloc{m_Pimpl->GetAllocationCallbacks(), 8};
     Vector<size_t*> allocatedItems{m_Pimpl->GetAllocationCallbacks()};
     for(size_t i = 0; i < 1000; ++i)
@@ -887,6 +1346,21 @@ void Allocator::Test()
         const size_t index = (size_t)rand() % allocatedItems.size();
         poolAlloc.Free(allocatedItems[index]);
         allocatedItems.remove(index);
+    }
+#endif
+
+    struct FooStruct { int foo[10]; };
+    List<FooStruct> list(m_Pimpl->GetAllocationCallbacks());
+    for(size_t i = 0; i < 1000; ++i)
+    {
+        list.PushBack();
+    }
+    while(!list.empty())
+    {
+        if(rand() & 0x80)
+            list.PopBack();
+        else
+            list.PopFront();
     }
 }
 
